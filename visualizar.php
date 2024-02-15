ea3fbc5bc3d90321718a9ee9fb3e97aedc597092
Brunoquerinojr <brunoquerinosilvajr@gmail.com>
2024-02-15 07:53:42 -0300

    attfinal

--- visualizar.php
+++ visualizar.php
@@ -1,31 +1,36 @@
 <?php
-// conexão com banco de dados
+
+// Incluir a conexao com o banco de dados
 include_once "conexao.php";
 
-// Receber a página via método GET
-$id = filter_input(INPUT_GET, "id", FILTER_SANITIZE_NUMBER_INT);
+// Receber o ID via método GET
+$id = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_NUMBER_INT);
 
-// Verificar se o ID não está vazia
+// Verifica se o ID não está vazio
 if (!empty($id)) {
-    // Selecionar usuario e endereço
-    $query_usuarios = "SELECT usr.id, usr.nome, usr.email,
-        ende.logradouro, ende.numero
-        FROM usuarios AS usr
-        LEFT JOIN enderecos AS ende ON ende.usuario_id=usr.id
-        WHERE usr.id=:id LIMIT 1";
-    $result_usuarios = $conn->prepare($query_usuarios);
-    $result_usuarios->bindParam(':id', $id);
-    $result_usuarios->execute();
+    // Query para selecionar informações do usuário e endereço associado
+    $query_usuario = "SELECT usr.id, usr.nome, usr.email,
+            ende.logradouro, ende.numero
+            FROM usuarios AS usr
+            LEFT JOIN enderecos AS ende ON ende.usuario_id=usr.id
+            WHERE usr.id=:id LIMIT 1";
+    $result_usuario = $conn->prepare($query_usuario);
+    $result_usuario->bindParam(':id', $id);
+    $result_usuario->execute();
 
-    // Verifica se a consulta foi bem sucedida e tem registro
-    if (($result_usuarios) and ($result_usuarios->rowCount() !=0)) {
-        $row_usuario = $result_usuarios->fetch(PDO::FETCH_ASSOC);
-        $retorna = ['status' => true, 'dados' => $row_usuario]; 
+    // Verifica se a consulta foi bem-sucedida e se há registros
+    if (($result_usuario) and ($result_usuario->rowCount() != 0)) {
+        // Recupera os dados do usuário e endereço associado
+        $row_usuario = $result_usuario->fetch(PDO::FETCH_ASSOC);
+        $retorna = ['status' => true, 'dados' => $row_usuario];
     } else {
+        // Se não houver registros, retorna uma mensagem de erro
         $retorna = ['status' => false, 'msg' => "<div class='alert alert-danger' role='alert'>Erro: Nenhum usuário encontrado!</div>"];
     }
 } else {
+    // Se o ID estiver vazio, retorna uma mensagem de erro
     $retorna = ['status' => false, 'msg' => "<div class='alert alert-danger' role='alert'>Erro: Nenhum usuário encontrado!</div>"];
 }
-// Retornar o resultado em formato JSON
+
+// Retorna o resultado em formato JSON
 echo json_encode($retorna);
